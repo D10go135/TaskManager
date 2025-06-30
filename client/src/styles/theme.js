249e899b2d903373338318997916448afa585a8e
D10go135 <141785229+D10go135@users.noreply.github.com>
2025-06-30 15:22:20 -0300

    Update theme.js

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#00DC1F',
     },
     secondary: {
       main: '#dc004e',
